15a6bc6ba03b980a982f31670dab40b491d335fe
mandi-riegle <mandi.riegle@gmail.com>
2022-04-11 17:22:45 -0700

    import bootstrap + js
    imported 3rd party bootstrap and js files

---
+++ main.js
@@ -0,0 +1,63 @@
+(function() {
+    "use strict";
+  
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+      el = el.trim()
+      if (all) {
+        return [...document.querySelectorAll(el)]
+      } else {
+        return document.querySelector(el)
+      }
+    }
+  
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+      let selectEl = select(el, all)
+  
+      if (selectEl) {
+        if (all) {
+          selectEl.forEach(e => e.addEventListener(type, listener))
+        } else {
+          selectEl.addEventListener(type, listener)
+        }
+      }
+    }
+  
+    /**
+     * Scrolls to an element with header offset
+     */
+    const scrollto = (el) => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      })
+    }
+  
+    /**
+     * Mobile nav toggle
+     */
+    on('click', '.mobile-nav-toggle', function(e) {
+      select('#navbar').classList.toggle('navbar-mobile')
+      this.classList.toggle('bi-list')
+      this.classList.toggle('bi-x')
+    })
+  
+    /** Job title cursor slider */
+    const typed = select('.typed')
+    if (typed) {
+      let typed_strings = typed.getAttribute('data-typed-items')
+      typed_strings = typed_strings.split(',')
+      new Typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+      });
+    }
+  
